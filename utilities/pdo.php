bccd952ee04fd17eb3ad97ce83511200efe9e7fc
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-15 10:01:53 +0000

    Changed database name

--- utilities/pdo.php
+++ utilities/pdo.php
@@ -1,6 +1,6 @@
 <?php
 //use port 8889 on a mac
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=misc', 
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=veloworld', 
    'bobby', 'qwerty');
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
