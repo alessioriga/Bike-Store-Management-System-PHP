08f20e2f63f5f24757d18b2d4d8efb62d80d12a8
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-21 12:08:00 +0000

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    
+    if (!menuToggle || !navLinks) return;
+
+    menuToggle.addEventListener('click', function () {
+        navLinks.classList.toggle('open');
+    });
+
+});
+
